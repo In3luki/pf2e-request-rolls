6c9f717dab461e7ad7173dac18bf2c6ed5e25202
In3luki <41452412+In3luki@users.noreply.github.com>
2025-03-19 08:53:41 +0100

    Add `Select All` checkbox to player selection

--- src/module/apps/select-players-dialog/select-players.svelte
+++ src/module/apps/select-players-dialog/select-players.svelte
@@ -2,9 +2,20 @@
     import type { SelectPlayersDialogContext } from "./select-players.ts";
 
     const props: SelectPlayersDialogContext = $props();
+
+    function onSelectAllClick(event: MouseEvent & { currentTarget: HTMLInputElement }): void {
+        for (const player of props.state.players) {
+            player.checked = event.currentTarget.checked;
+        }
+    }
 </script>
 
 <div class="players">
+    <div class="form-group">
+        <label for="select-all">{game.i18n.localize("PF2ERequestRolls.SelectPlayersDialog.SelectAllLabel")}</label>
+        <input type="checkbox" id="select-all" checked onclick={onSelectAllClick} />
+    </div>
+    <hr />
     {#each props.state.players as player (player.id)}
         <div class="form-group">
             <label for={player.id}>{player.name}</label>
@@ -22,5 +33,9 @@
     .players {
         display: flex;
         flex-direction: column;
+
+        hr {
+            margin: 0.2rem 0;
+        }
     }
 </style>
